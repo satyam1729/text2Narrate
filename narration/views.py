5a1771f8e80f907e6672bcde31d9c58108e84b1e
Satyam Yadav <satyamyadav13579@gmail.com>
2018-01-27 02:38:03 +0530

    added tachotron for text-to-speech

--- narration/views.py
+++ narration/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from .forms import ContentForm
 from django.utils import timezone
+from hparams import hparams, hparams_debug_string
+from synthesizer import Synthesizer
+
 # Create your views here.
+
+#print(type(post.title))            
+synthesizer = Synthesizer()
+hparams.parse("")
+checkpoint="/home/satyam/code/text2Narrate/narration/saved_model/tacotron-20170720/model.ckpt"
+synthesizer.load(checkpoint)
+
+
 def home(request):
     if request.method == "POST":
         form=ContentForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.published_date = timezone.now()
-            post.save()    
+            synthesizer.synthesize(post.text)
+            post.created_date = timezone.now()
+            post.save()
     else:    
         form=ContentForm()
     return render(request, "narration/home.html", {"form":form})
