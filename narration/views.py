a0629c79a9f24aef97295bff5d31dc0a42dff0ac
Satyam Yadav <satyamyadav13579@gmail.com>
2018-01-26 22:57:36 +0530

    form and database integrated

--- narration/views.py
+++ narration/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .forms import ContentForm
-
+from django.utils import timezone
 # Create your views here.
 def home(request):
-    form=ContentForm()
+    if request.method == "POST":
+        form=ContentForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.save()    
+    else:    
+        form=ContentForm()
     return render(request, "narration/home.html", {"form":form})
