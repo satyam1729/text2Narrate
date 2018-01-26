69d44beae6a723ec7a4a2e69fc44651d9f9768bc
Satyam Yadav <satyamyadav13579@gmail.com>
2018-01-26 19:42:33 +0530

    added the form for content

--- narration/views.py
+++ narration/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .forms import ContentForm
 
 # Create your views here.
+def home(request):
+    form=ContentForm()
+    return render(request, "narration/home.html", {"form":form})
